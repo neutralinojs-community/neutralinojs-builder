4d38e4777142c679911c1f645146ee2a04e38e64
SharonIV0X86 <anirudhlakhanpal123@gmail.com>
2026-05-19 00:07:43 +0530

    fix: spacing

--- constants/buildargs.js
+++ constants/buildargs.js
@@ -1,33 +1,22 @@
 const cliOptions = {
-
     target: null,
-
     arch: null,
-
     output: null
-
 };
 
 const resolvedConfig = {
 
     hostPlatform: null,
-
     targetPlatform: null,
-
     target: null,
-
     arch: "x64",
-
     assets: {},
-
     metadata: {},
-
     paths: {}
 
 };
 
 module.exports = {
-
     cliOptions,
     resolvedConfig
 
