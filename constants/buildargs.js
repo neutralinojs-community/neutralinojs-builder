461dfae1d25db8dcf3f8145def9e402e2d352577
SharonIV0X86 <anirudhlakhanpal123@gmail.com>
2026-05-19 00:02:12 +0530

    feat: Added CLI + Config file parsing and a basic .gitignore

---
+++ constants/buildargs.js
@@ -0,0 +1,34 @@
+const cliOptions = {
+
+    target: null,
+
+    arch: null,
+
+    output: null
+
+};
+
+const resolvedConfig = {
+
+    hostPlatform: null,
+
+    targetPlatform: null,
+
+    target: null,
+
+    arch: "x64",
+
+    assets: {},
+
+    metadata: {},
+
+    paths: {}
+
+};
+
+module.exports = {
+
+    cliOptions,
+    resolvedConfig
+
+};
--- index.js
+++ index.js
@@ -1,13 +1,28 @@
-#!/usr/bin/env node
-const logger = require('./utils/logger');
+
+const logger = require("./utils/logger");
+const resolveConfig = require("./lib/configresolver");
 
 const args = process.argv.slice(2);
+
 const target = args[0];
 
 logger.info("Neutralinojs Builder initialized.");
 
+const config = resolveConfig();
+
+logger.info("Configuration loaded.");
+
+console.log(config);
+
 if (!target) {
-    logger.warn("No target specified. Usage: neu-builder <target>");
-} else {
+
+    logger.warn(
+        "No target specified. Usage: neu-builder <target>"
+    );
+
+}
+else {
+
     logger.info(`Target detected: ${target}`);
+
 }
