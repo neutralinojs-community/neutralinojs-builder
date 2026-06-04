f9ee4309616216dbf3969280d9d318430c623430
subhu339 <sk3656770@gmail.com>
2026-06-04 14:53:47 +0530

    feat: add target archs matrix and fix nsis x64 limits

--- constants/targets.js
+++ constants/targets.js
@@ -3,23 +3,27 @@ const targets = {
         targetPlatform: "linux",
         requiredTools: ["dpkg-deb"],
         supportedHosts: ["linux", "mac"],
-        supportedViaWSL: ["windows"]
+        supportedViaWSL: ["windows"],
+        supportedArchitectures: ["x64", "armhf", "arm64"]
     },
     appimage: {
         targetPlatform: "linux",
         requiredTools: ["appimagetool"],
         supportedHosts: ["linux", "mac"],
-        supportedViaWSL: ["windows"]
+        supportedViaWSL: ["windows"],
+        supportedArchitectures: ["x64", "arm64"]
     },
     nsis: {
         targetPlatform: "windows",
         requiredTools: ["makensis"],
-        supportedHosts: ["linux", "windows", "mac"]
+        supportedHosts: ["linux", "windows", "mac"],
+        supportedArchitectures: ["x64"]
     },
     dmg: {
         targetPlatform: "mac",
         requiredTools: ["hdiutil"],
-        supportedHosts: ["mac"]
+        supportedHosts: ["mac"],
+        supportedArchitectures: ["x64", "arm64", "universal"]
     }
 };
 
